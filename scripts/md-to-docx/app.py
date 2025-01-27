ced27dd3189dcebc2c60b489e1d515558dcf8caa
Spencer Strelsov <sstrelsov@gmail.com>
2025-01-26 21:19:05 -0500

    removes reference doc at finish

--- scripts/md-to-docx/app.py
+++ scripts/md-to-docx/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
 import yaml
-# Import the refactored helper functions
 from doc_utils.overrides import convert_md_to_word, create_reference_doc
 from doc_utils.table_style import apply_table_style
 
@@ -27,23 +27,29 @@ def main():
 
     reference_docx = "reference.docx"
 
-    # 1) Create reference doc with styles
-    create_reference_doc(config, reference_docx)
+    try:
+        # 1) Create reference doc with styles
+        create_reference_doc(config, reference_docx)
 
-    # 2) Convert from Markdown to Word using Pandoc
-    convert_md_to_word(
-        input_md,
-        output_docx,
-        reference_docx,
-        from_format=config.get("pandoc_options", {}).get("from", "markdown+footnotes+mark")
-    )
+        # 2) Convert from Markdown to Word using Pandoc
+        convert_md_to_word(
+            input_md,
+            output_docx,
+            reference_docx,
+            from_format=config.get("pandoc_options", {}).get("from", "markdown+footnotes+mark")
+        )
 
-    # 3) Apply table style (if you want to style tables in the final doc)
-    #    If 'pandoc_options.table_style' not set, default to "Light Shading".
-    table_style = config.get("pandoc_options", {}).get("table_style", "Light Shading")
-    apply_table_style(output_docx, table_style, output_docx)
+        # 3) Apply table style (if you want to style tables in the final doc)
+        #    If 'pandoc_options.table_style' not set, default to "Light Shading".
+        table_style = config.get("pandoc_options", {}).get("table_style", "Light Shading")
+        apply_table_style(output_docx, table_style, output_docx)
 
-    print(f"[INFO] Finished creating {output_docx} with reference doc {reference_docx}")
+        print(f"[INFO] Finished creating {output_docx} with reference doc {reference_docx}")
+    finally:
+        # Remove the reference doc to avoid clutter
+        if os.path.exists(reference_docx):
+            os.remove(reference_docx)
+            print(f"[INFO] Deleted the reference DOCX: {reference_docx}")
 
 
 if __name__ == "__main__":
